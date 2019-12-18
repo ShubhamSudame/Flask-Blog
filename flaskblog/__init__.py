ba623a6677bc7bc4f53d1e9e00e1f463e0b28d09
sudrox97 <shubham.sudame@gmail.com>
2019-12-18 10:03:52 +0530

    Log In and Log Out

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_hashing import Hashing
+from flask_login import LoginManager
 
 app = Flask(__name__)
 # Secret key will protect against modifying cookies or cross-site request forgery attacks
@@ -8,7 +9,9 @@ app.config['SECRET_KEY'] = '6b7e7f17628fb3d6ecf968aedec3e844'
 # Create a configuration of database by providing SQLAlchemy the path URI of the database.
 # We're using SQLite for developement, and migrate to Postgres in production.
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 hashing = Hashing(app)
+login_manager = LoginManager(app)
 
 from flaskblog import routes
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from flaskblog import app, db, hashing
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
+from flask_login import login_user, current_user, logout_user
 
 db.create_all()
 # Such JSON data needs to be retrieved from database, or through APIs
@@ -56,34 +57,55 @@ posts = [
     }
 ]
 
+
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html', title='Home Page', posts=posts)
 
+
 @app.route('/about', methods=['GET'])
 def about():
     return render_template('home.html', title='About Page')
 
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = hashing.hash_value(form.password.data)
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        user = User(username=form.username.data,
+                    email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and hashing.check_value(user.password, form.password.data) == True:
+            login_user(user, form.remember.data)
             return redirect(url_for('home'))
         else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
+            flash('Login. Please check username and password', 'danger')
     return render_template('login.html', title='Log In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route('/account')
+def account():
+    return render_template('account.html', title='Account')
