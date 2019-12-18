c0c76376f93728e094ca9e00a0434bb1550a6084
sudrox97 <shubham.sudame@gmail.com>
2019-12-18 07:44:46 +0530

    Models

--- app.py
+++ app.py
@@ -1,9 +1,38 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 # Secret key will protect against modifying cookies or cross-site request forgery attacks
 app.config['SECRET_KEY'] = '6b7e7f17628fb3d6ecf968aedec3e844'
+# Create a configuration of database by providing SQLAlchemy the path URI of the database.
+# We're using SQLite for developement, and migrate to Postgres in production.
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(20), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='static/default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True) 
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+
 # Such JSON data needs to be retrieved from database, or through APIs
 posts = [
     {
